fd71c041599f34159920d892ba21df0b70ac1506
Jeremy Waller <jeremyw@phase2online.com>
2018-10-02 19:37:34 -0500

    Add Avery 5160 label and support for letter-sized labels (#1)

---
+++ Labels/LetterLabels/Avery/L5160.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpPDFLabel.Labels.LetterLabels.Avery
+{
+    public class L5160 : LabelDefinition
+    {
+        // all sizes are in mm
+        public L5160()
+        {
+            _Width = 66.802;
+            _Height = 25.4;
+            _HorizontalGapWidth = 3.048;
+            _VerticalGapHeight = 0;
+
+            _PageMarginTop = 12.7;
+            _PageMarginBottom = 12.7;
+            _PageMarginLeft = 4.826;
+            _PageMarginRight = 4.826;
+
+            PageSize = Enums.PageSize.LETTER;
+            LabelsPerRow = 3;
+            LabelRowsPerPage = 10;
+        }
+    }
+}
